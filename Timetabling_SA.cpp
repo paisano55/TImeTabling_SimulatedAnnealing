cf8713f7386f68289ef3976ec1456a3fd1191743
paisano55 <paisano55@naver.com>
2018-09-29 15:32:14 +0900

    Working on initialize

--- Timetabling_SA.cpp
+++ Timetabling_SA.cpp
@@ -1,23 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 #include <math.h>
 #include <vector>
-#include <iostream>
+#include <time.h>
 
-int timeTable[5][9][2];
-int timeTableSrc[5][9][2]; // Save the code of group
+int timeTable[5][9][2] = { 0 }; // 0 : 할당된 그룹없음 , -1 : 수업이 없는 시간 (연구활동, 방과후 etc.)
+int timeTableSrc[5][9][2] = { 0 }; // Save the code of group
 int fitNow, fitSrc;
 
 int main(void)
 {
-	struct subject
+	struct subjectBase
 	{
 		vector<int> student;
 		int teacher, subcode, floor, position, time;
 	};
+	struct groupBase
+	{
+		vector <subjectBase> subject;
+		groupCode;
+		maxTime;
+		vector <int> student;
+		vector <int> teacher;
+	};
 	vector <subject> group;
-	int L,looped,fit
-	double T=100,dec
-	initial(); //create initial timetable
+	int L, looped, fit
+	double T = 100, dec;
+	initialize();
+	initTimeTable(); //create initial timetable
 	fitNow=fitness(0)
 
 	while(T>0.01)
@@ -42,11 +53,21 @@ int fitness(int src) // src=0 : 현재해 적합도 src=1 이웃해 적합도
 		fitTimeTable[5][9][2] = timeTable[5][9][2];
 	}
 }
-int checking()
+int checking(int src)
 {
+	int chkTimeTable[5][9][2];
+	int chk = 1;
+	if (src)
+	{
+		chkTimeTable[5][9][2] = timeTableSrc[5][9][2];
+	}
+	else
+	{
+		chkTimeTable[5][9][2] = timeTable[5][9][2];
+	}
 	for (int i = 0; i < 5; i++)
 	{
-		for (int j = 0; j < 9; j++)
+		for (int j = 0; chkTimeTable[i][j][0]!= 0 && chkTImeTable[i][j][1]!=0; j++)
 		{
 			
 		}
@@ -56,7 +77,15 @@ void search()
 {
 
 }
-int initial()
+int initTimeTable()
 {
+
+}
+void initialize()
+{
+	timeTable[0][7][0], timeTable[0][7][1] = -1;
+	timeTable[2][5][0], timeTable[2][5][1], timeTable[2][6][0], timeTable[2][6][1], timeTable[2][7][0], timeTable[2][7][1], timeTable[2][8][0], timeTable[2][8][1] = -1;
+	timeTable[4][5][0], timeTable[4][5][1], timeTable[4][6][0], timeTable[4][6][1], timeTable[4][7][0], timeTable[4][7][1] = -1;	
+	
 
 }
